ec77e830179c7dda0e0bc31d2390a5ab2a937caa
Mark Karpeles <magicaltux@gmail.com>
2023-06-14 20:26:45 +0900

    use rpctest external package

--- rpc_test.go
+++ rpc_test.go
@@ -1,94 +1,20 @@
 package clouddb_test
 
 import (
-	"context"
 	"fmt"
-	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/KarpelesLab/clouddb"
+	"github.com/KarpelesLab/rpctest"
 )
 
-type fakeRPC interface {
-	clouddb.RPC
-	run([]byte) ([]byte, error)
-}
-
-type rpcSyncPool map[string]*rpcSyncPeer
-
-func (p rpcSyncPool) newPeer(id string) clouddb.RPC {
-	n := &rpcSyncPeer{
-		pool: p,
-		id:   id,
-	}
-	p[id] = n
-	return n
-}
-
-type rpcSyncPeer struct {
-	pool   rpcSyncPool
-	id     string
-	target func(context.Context, []byte) ([]byte, error)
-}
-
-func (r *rpcSyncPeer) All(ctx context.Context, data []byte) ([]any, error) {
-	var res []any
-	for _, p := range r.pool {
-		r, e := p.run(ctx, data)
-		if e != nil {
-			res = append(res, e)
-		} else {
-			res = append(res, r)
-		}
-	}
-	return res, nil
-}
-
-func (r *rpcSyncPeer) Broadcast(ctx context.Context, data []byte) error {
-	for _, p := range r.pool {
-		p.run(ctx, data)
-	}
-	return nil
-}
-
-func (r *rpcSyncPeer) Request(ctx context.Context, id string, data []byte) ([]byte, error) {
-	p, ok := r.pool[id]
-	if !ok {
-		return nil, fs.ErrNotExist
-	}
-	return p.run(ctx, data)
-}
-
-func (r *rpcSyncPeer) Send(ctx context.Context, id string, data []byte) error {
-	if p, ok := r.pool[id]; ok {
-		p.run(ctx, data)
-	}
-	return nil
-}
-
-func (r *rpcSyncPeer) Self() string {
-	return r.id
-}
-
-func (r *rpcSyncPeer) Connect(cb func(context.Context, []byte) ([]byte, error)) {
-	r.target = cb
-}
-
-func (r *rpcSyncPeer) run(ctx context.Context, data []byte) ([]byte, error) {
-	t := r.target
-	if t != nil {
-		return t(ctx, data)
-	}
-	return nil, fs.ErrNotExist
-}
-
 func TestSyncRPC(t *testing.T) {
-	rpc := make(rpcSyncPool)
-	rpca := rpc.newPeer("a")
-	rpcb := rpc.newPeer("b")
+	rpc := rpctest.NewSync()
+	rpca := rpc.NewPeer("a")
+	rpcb := rpc.NewPeer("b")
 
 	// create a local db, test stuff
 	p := filepath.Join(os.TempDir(), fmt.Sprintf("clouddb-test-%d", os.Getpid()))
