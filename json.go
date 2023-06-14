2cff7be8aaeaf10be9017d16885d5c5492ffa7f6
Mark Karpeles <magicaltux@gmail.com>
2023-06-14 20:08:28 +0900

    add compare method

--- json.go
+++ json.go
@@ -42,3 +42,48 @@ func ApplyUpdate(obj any, upd any) (any, error) {
 		return upd, nil
 	}
 }
+
+// ComputeDelta compares two objects (of type map[string]any) and returns an
+// object that contains all the changes found in now while skipping any value
+// that might be equal.
+func ComputeDelta(old, now any) (any, error) {
+	if old == nil {
+		return now, nil
+	}
+	if now == nil {
+		return nil, nil
+	}
+
+	switch o := old.(type) {
+	case map[string]any:
+		u, ok := now.(map[string]any)
+		if !ok {
+			return nil, fmt.Errorf("expected map[string]any now, got %T", now)
+		}
+
+		delta := make(map[string]any)
+
+		for k, v := range u {
+			prev, ok := o[k]
+			if !ok {
+				// new value
+				delta[k] = v
+				continue
+			}
+			if v == prev {
+				// will this work for stuff like map[string]any? ideally we want such cases to always return false
+				continue
+			}
+			if nd, err := ComputeDelta(prev, v); err != nil {
+				return nil, err
+			} else if m, ok := nd.(map[string]any); ok && len(m) == 0 {
+				// do nothing
+			} else {
+				delta[k] = nd
+			}
+		}
+		return delta, nil
+	default:
+		return now, nil
+	}
+}
